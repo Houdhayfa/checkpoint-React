04fbeb1bea32e930e3b1dee01575d078ccf9c87a
Houdhayfa <benkhlil.houdhayfa@gmail.com>
2020-08-16 17:35:39 +0100

    css modification

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,7 +4,7 @@ function MainContent() {
     return(
         <main>
             <div className="main-content">
-            <h1>Introduce yourself</h1>
+            <h1 id="intro">Introduce yourself</h1>
             <Form/>
             </div>
         </main>
